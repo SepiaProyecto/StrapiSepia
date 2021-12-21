cc3afca624a548320215149ebc058a22b577c1c6
ServerSepia <neto1998.mtz@hotmail.com>
2021-12-20 21:57:05 -0600

    nuevos

--- config/database.js
+++ config/database.js
@@ -5,14 +5,17 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', '10.147.20.1'),
+        host: env('DATABASE_HOST', 'ec2-52-200-188-218.compute-1.amazonaws.com'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'bd_cursox'),
-        username: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'root_01'),
-        ssl: env.bool('DATABASE_SSL', false),
+        database: env('DATABASE_NAME', 'db90qkruentgr7'),
+        username: env('DATABASE_USERNAME', 'mtszodzsmarawp'),
+        password: env('DATABASE_PASSWORD', '3cbc1a693a6f412f333737afa5d9d53865d71b82a98f2b469410a29698f2bbea'),
+        ssl:{rejectUnauthorized:env.bool('DATABASE_SSL_SELF',false)
       },
-      options: {}
+      },
+      options: {ssl:env.bool('DATABASE_SSL',false),
+    },
+    options:{}
     },
   },
 });
